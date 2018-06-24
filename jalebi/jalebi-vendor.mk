7fad69ffc63d3cf228db79bbf290f553ef954274
Michael Bestas <mkbestas@lineageos.org>
2018-06-24 21:19:50 +0300

    jalebi: Remove btnvtool
    Change-Id: Icd261ece7de7cbf208fd2215dee5f8f1f5a98576

--- jalebi/jalebi-vendor.mk
+++ jalebi/jalebi-vendor.mk
@@ -17,7 +17,6 @@
 
 PRODUCT_COPY_FILES += \
     vendor/yu/jalebi/proprietary/vendor/bin/adsprpcd:system/vendor/bin/adsprpcd \
-    vendor/yu/jalebi/proprietary/vendor/bin/btnvtool:system/vendor/bin/btnvtool \
     vendor/yu/jalebi/proprietary/vendor/bin/hci_qcomm_init:system/vendor/bin/hci_qcomm_init \
     vendor/yu/jalebi/proprietary/vendor/bin/irsc_util:system/vendor/bin/irsc_util \
     vendor/yu/jalebi/proprietary/vendor/bin/mm-pp-daemon:system/vendor/bin/mm-pp-daemon \
